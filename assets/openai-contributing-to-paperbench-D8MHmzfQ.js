369f17a32178faca84468e3e3563f1743854fffe
Tim <cs.timknappe@gmail.com>
2025-11-04 21:17:31 +0100

    Updates

--- assets/openai-contributing-to-paperbench-D8MHmzfQ.js
+++
@@ -1,4 +0,0 @@
-const e=`I was fortunate to cooperate with OpenAI, in the creation of PaperBench in which the paper I co-authored is featured in.\r
-\r
-Out work is in the dev-set of the dataset.\r
-`;export{e as default};
---
+++ assets/what-ai-does-to-our-brain-dPxsPLL9.js
@@ -0,0 +1,2 @@
+const e=`This is **Markdown** content rendered in React!\r
+`;export{e as default};
